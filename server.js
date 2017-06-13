48498c409403418c496d1f9872dabafd689dac03
Jimel J. Joseph <jimelj@gmail.com>
2017-06-13 13:17:09 -0400

    deploying app

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.Promise = Promise;
 
 //Database Config with mongoose
 if (process.env.MONGODB_URI) {
-  mongoose.connect('mongodb://heroku_n9mdksz4:cn2fgs7ivlb67qebmlcvg44v2u@ds127132.mlab.com:27132/heroku_n9mdksz4');
+  mongoose.connect('process.env.MONGODB_URI');
 } else {
   mongoose.connect('mongodb://localhost/theScraper');
 }
